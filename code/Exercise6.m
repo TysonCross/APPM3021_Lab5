8d8d4933c2cda61b5bd8a788b3f1055cccbf5e07
Tyson <tyson.cross@gmail.com>
2018-05-23 08:32:07 +0200

    Exercise 6

--- code/Exercise6.m
+++ code/Exercise6.m
@@ -1,13 +1,13 @@
 % APPM3021 Lab 5 Exercise 6
 % Tyson Cross 1239448
 
-clc; clear all; set(0,'ShowHiddenHandles','on'); delete(get(0,'Children'));
+clc; clear all;
 
 % sindex = @(A, s) A(s);     % An anonymous function for 2-D indexing
 % value = sindex(f, 1);  % Use the function to index the matrix
 
 N = 30;
-xspan = linspace(0,5,N);
+xspan = [0 5];
 y0 = [0 0 1];
 
 %{
@@ -23,14 +23,13 @@ y0 = [0 0 1];
 %}
 
 % function handles
-f = @(x,y) [y(3),y(3),-y(3)];
+f = @(x,y) [y(2);y(3);-y(3)];
 
-% f1 = @(x,y1,y2,y3)  y2;
-% f2 = @(x,y1,y2,y3)  y3;
-% f3 = @(x,y1,y2,y3) -y3;
-% 
-% f = {f1,f2,f3};
-
+% syms x y(x);
+% D3y = diff(y,x,3);
+% D2y = diff(y,x,2);
+% D1y = diff(y,x);
+% F = D3y + D2y == 0;
 function_name = 'y'''''' + y'''' = 0';
 
 %% Calculations
@@ -40,25 +39,28 @@ fprintf('Calculating...');                      tic;
 [X,Y] = RK4system(f,xspan,y0,N);            	fprintf('.');
 
 % Confirm exact solution
+tspan = linspace(xspan(1),xspan(end),N+1)';
+[X_sol, Y_sol] = ode45(f, tspan, y0);           fprintf('.');
 
-[X_Solution, Y_solution] = ode45(f, xspan, y0); fprintf('.');
-% [~,Y_solution,sol] = ExactODE(f,x0,y0,N,xf);    
-
-rk4_sol = double(f(X,Y));                       fprintf('.');
-exact_sol = double(f(X_solution,Y_solution));   fprintf('.');
-err = abs((exact_sol - rk4_sol)./exact_sol);    fprintf('.');
+% sol = double(F(X,Y(:,1)))';                  	fprintf('.');
+% exact_sol = double(F(X_sol,Y_sol(:,1))');       fprintf('.');
+% err = abs((exact_sol - rk4_sol)./exact_sol);    fprintf('.');
 
 % Display results
 disp(' ')
 disp(['N = ', num2str(N)]);
-disp(['h = ', num2str(xf/N)]);
-disp(['x is an element of [', num2str(x0), ',', num2str(xf), ']' ]);
+disp(['h = ', num2str((xspan(end)-xspan(1))/N)]);
+disp(['x is an element of [', num2str(xspan), ']' ]);
 disp(['f(x,y) = ', function_name]);
-disp(['Exact Solution: y = ', char(sol)]);
+% disp(['Exact Solution: y = ', char(sol)]);
 disp(' ')
-T = table (X, Y, rk4_sol, exact_sol, err);
-T.Properties.VariableNames = {'x','y','f','Exact','RE'};
-T.Properties.Description = 'Runge-Kutta Method';
-disp(['            ',T.Properties.Description, ':']);
-disp(' ');
-disp(T)
+% T = table (X, Y, rk4_sol, exact_sol, err);
+% T.Properties.VariableNames = {'x','y','f','Exact','RE'};
+% T.Properties.Description = 'Runge-Kutta System Method';
+% disp(['            ',T.Properties.Description, ':']);
+% disp(' ');
+% disp(T)
+disp('X=')
+disp(X);
+disp('Y=')
+disp(Y)
--- code/RK4system.m
+++ code/RK4system.m
@@ -7,41 +7,22 @@ function [X,Y] = RK4system(f,xspan,y0,N)
 
 x0 = xspan(1);
 xf = xspan(end);
-
-% % initial conditions
-% for ii=1:numel(y0)
-%     DnY(ii) = diff(y,x,ii) == y0(ii);
-% end
-
-% reduce to system of 1st order ODE
 n = numel(y0);
-% g(y) = str2sym('y(2)');
+h = (xf-x0)/N;
 
-X = zeros(N+1,1);
+X = linspace(xspan(1),xspan(end),N+1)';
 Y = zeros(N+1,n);
+Y(1,:) = y0;
 
-X(1,1) = x0;
-Y(1,1) = y0(1);
+% solve with RK4, as matrix
 
-% solve with RK4()
-% syms x y1 y2 y3
-% f1 = f{1};
-% f2 = f{2};
-% f3 = f{3};
-% G = [f1(u,y1,y2,y3), f2(u,y1,y2,y3), f3(u,y1,y2,y3)];
-G = f(x,y);
-H = symfun(G,symvar(G));
-
-
-
-% for ii=1:numel(G)
-%     F{ii} = symfun(G(ii),symvar(G(ii)));
-% end
-
-% H = symfun(G,[x y]);
-
-for ii=2:n+1
-    [X(ii,1),Y(ii,:)] = RK4(H,x0,y0(ii-1),N,xf);
+for ii=2:N+1
+    k = ii-1;
+    k1 = h*f(X(k),Y(k,:))';
+    k2 = h*f(X(k) + (h/2), Y(k,:) + (k1./2))';
+    k3 = h*f(X(k) + (h/2), Y(k,:) + (k2./2))';
+    k4 = h*f(X(k) + h, Y(k,:) + k3)';
+	Y(ii,:) = Y(k,:) + (1/6)*(k1 + 2.*k2 + 2.*k3 + k4);
 end
 
 end
